7135fbfd44ac55c016dea59ce4d83c4d5dd2c82e
babisha0123 <babishab025@gmail.com>
2026-03-10 20:57:39 +0530

    fix vercel express server

---
+++ api/[...path].js
@@ -0,0 +1,24 @@
+const serverless = require("serverless-http");
+const app = require("../src/app");
+const { connectDb } = require("../src/config/db");
+
+let handler;
+
+module.exports = async (req, res) => {
+  try {
+    await connectDb();
+
+    if (!handler) {
+      handler = serverless(app);
+    }
+
+    return handler(req, res);
+
+  } catch (error) {
+    console.error("Server error:", error);
+
+    return res.status(500).json({
+      message: "Internal server error"
+    });
+  }
+};
